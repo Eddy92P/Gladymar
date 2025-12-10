c66c646a907e3f82d9ee7d73c5baeaf1e3a71c5a
Eddy Peñaranda <edpe1992@gmail.com>
2025-12-09 23:14:22 -0400

    fix requirements for deploy

--- backend/start.sh
+++ backend/start.sh
@@ -8,4 +8,4 @@ echo "Recolectando staticfiles..."
 python manage.py collectstatic --noinput
 
 echo "Iniciando Gunicorn..."
-gunicorn core.wsgi:application --bind 0.0.0.0:$PORT --workers 3
+/py/bin/gunicorn app.wsgi:application --bind 0.0.0.0:$PORT --workers 3
