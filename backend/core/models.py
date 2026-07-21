c9ba68c60a09f22a4b3c793533f5ded0ace6fb80
Eddy92P <edpe1992@gmail.com>
2026-07-21 10:45:44 -0400

    increase email length

--- backend/core/models.py
+++ backend/core/models.py
@@ -154,7 +154,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         ]
     )
     email = models.EmailField(
-        max_length=20,
+        max_length=60,
         unique=True,
         null=False,
         blank=False)
